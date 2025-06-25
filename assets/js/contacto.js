d231fc03b68cff8e9ebc5f1b9bde5a908d19dcfb
domixde <114755502+domixde@users.noreply.github.com>
2025-06-24 21:58:41 -0500

    Update contacto.js

--- assets/js/contacto.js
+++ assets/js/contacto.js
@@ -8,8 +8,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const email = document.querySelector("#email").value.trim();
     const mensaje = document.querySelector("#mensaje").value.trim();
 
-    // Reemplaza con tu número real de WhatsApp, sin símbolos, con código de país (por ejemplo: 5491123456789)
-    const numeroWhatsApp = "51928522337";
+    const numeroWhatsApp = "51917998856";
 
     const texto = `Hola, soy ${nombre}. Mi correo es ${email}. Quisiera decir lo siguiente:\n${mensaje}`;
     const url = `https://wa.me/${numeroWhatsApp}?text=${encodeURIComponent(texto)}`;
